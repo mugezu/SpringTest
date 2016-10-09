d0fb10092fecfaa8b6c935f367b23ead3e248ca6
Mugezu <mugezu2013@gmail.com>
2016-10-10 02:02:35 +0300

    Main edit2

--- src/A.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by user on 09.10.2016.
- */
-public interface A {
-    public void Hello();
-    public void KK();
-}
--- src/Main.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * Created by user on 09.10.2016.
- */
-public class Main {
-    public static void main(String[] args) {
-        //AbstractApplicationContext context = new ClassPathApplicationContext("config.xml");
-    }
-}
