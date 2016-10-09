987bb9ca8b45f05a4584efa796d2d5d901686ebe
Mugezu <mugezu2013@gmail.com>
2016-10-09 20:58:21 +0300

    Main edit2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,5 @@
 public class Main {
     public static void main(String[] args) {
         //AbstractApplicationContext context = new ClassPathApplicationContext("config.xml");
-        System.out.println("sdfg");
     }
 }
